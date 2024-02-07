ce5bd044fa59c4fcaf4493db8544b9249af28ac7
Mbaouz <145072570+Mbaouz@users.noreply.github.com>
2024-02-07 11:44:50 +0100

    fin html debut js

---
+++ FrontEnd/front.js
@@ -0,0 +1,8 @@
+const openModal = document.querySelector(".Bt-Modif");
+const modal= document.querySelector(".modal");
+openModal.addEventListener("click" , function () {
+    console.log("ok!!!!");
+    
+    modal.classList.innerHTML("display:");
+
+});
